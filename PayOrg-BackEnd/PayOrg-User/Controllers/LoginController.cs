dba9b44922c7e2e4bc027539c3a9561d1995db58
Regis <53189542+Regiss1@users.noreply.github.com>
2024-07-05 22:12:42 -0300

    MicroServiceUser

--- PayOrg-BackEnd/PayOrg-User/Controllers/LoginController.cs
+++ PayOrg-BackEnd/PayOrg-User/Controllers/LoginController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web.Http.Description;
-using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
